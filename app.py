2a7cc20427fe6be0b3dfcc2e09e7d7ee799df748
YFA22SCM49L <yli393@hawk.iit.edu>
2022-11-21 01:02:54 +0800

    Remove pulls and commits completely

--- app.py
+++ app.py
@@ -356,12 +356,6 @@ def github():
         "closedAtImageUrls": {
             **closed_at_response.json(),
         },
-        '''"pullsImageUrls": {
-            **pulls_at_response.json(),
-        },
-        "commitsImageUrls": {
-            **commits_at_response.json(),
-        },'''
     }
     # Return the response back to client (React app)
     return jsonify(json_response)
